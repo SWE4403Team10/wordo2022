bfc3935d5c54cdfbc40ffe81974b9816d242d84c
dunc415 <duncanchet2001@hotmail.com>
2022-03-03 18:06:12 -0400

    idk

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.example.wordo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.wordo to javafx.fxml;
+    exports com.example.wordo;
+}
